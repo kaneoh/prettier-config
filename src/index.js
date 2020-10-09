fa0fad144145cafb934bb93a6da8991bd22a0f76
Noe Charmet <noe@rolo.chat>
2020-10-09 12:20:17 +0200

    Initial commit`

---
+++ src/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  $schema: "http://json.schemastore.org/prettierrc",
+  singleQuote: true,
+  trailingComma: "all",
+  endOfLine: "lf",
+  bracketSpacing: false,
+  arrowParens: "always",
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 100,
+};
